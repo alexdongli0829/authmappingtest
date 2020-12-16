6814276f4685200e6ccc2162cd33c13d5943245d
dongaws <alexdongli0829@gmail.com>
2020-12-16 00:59:58 +0000

    update

--- NameTest.java
+++ NameTest.java
@@ -9,35 +9,6 @@ public class NameTest{
 private static final Pattern nameParser =
       Pattern.compile("([^/@]+)(/([^/@]+))?(@([^/@]+))?");
 
-private static final Pattern ruleParser =
-    Pattern.compile("\\s*((DEFAULT)|(RULE:\\[(\\d*):([^\\]]*)](\\(([^)]*)\\))?"+
-                    "(s/([^/]*)/([^/]*)/(g)?)?))/?(L)?");
-
-
-private List<Rule> parseRules(String rules) {
-    List<Rule> result = new ArrayList<Rule>();
-    String remaining = rules.trim();
-    while (remaining.length() > 0) {
-      Matcher matcher = ruleParser.matcher(remaining);
-      if (!matcher.lookingAt()) {
-        throw new IllegalArgumentException("Invalid rule: " + remaining);
-      }
-      if (matcher.group(2) != null) {
-        result.add(new Rule());
-      } else {
-        result.add(new Rule(Integer.parseInt(matcher.group(4)),
-                            matcher.group(5),
-                            matcher.group(7),
-                            matcher.group(9),
-                            matcher.group(10),
-                            "g".equals(matcher.group(11)),
-                            "L".equals(matcher.group(12))));
-      }
-      remaining = remaining.substring(matcher.end());
-    }
-    return result;
-  }
-
 public static void main(String[] args) throws Exception {
     
     String name = args[0];
